6e33dcd848da45e5eca778a0a7a6d80b1b343623
kzmna <kzmna.a@icloud.com>
2026-04-17 10:27:18 +0300

    add mesh

--- src/data_generator.py
+++ src/data_generator.py
@@ -1,19 +1,26 @@
 import torch
 
 def generate_dataset(
-    n_pde=10,
-    n_bc=10,
-    n_ic=10,
+    # == кол-во точек ==
+    n_pde=1000,
+    n_data=100,
+    n_bc=500,
+    n_ic=500,
+    n_test=100,
+
+    # == константы ==
     S_max=500.0,
     S0=50.0,
-    k=0.7,
-    v_m=0.16,
+    k=0.12,
+    v_m=0.2,
     t_max=1800.0,
     z_max=10.0,
-    noise_std=10.0
+
+    # == параметры ==
+    noise_std=10.0,
 ):
     # -------------------------
-    # PDE точки (внутри области)
+    # PDE
     # -------------------------
     t_pde = torch.rand(n_pde, 1) * t_max
     z_pde = torch.rand(n_pde, 1) * z_max
@@ -21,6 +28,7 @@ def generate_dataset(
     # -------------------------
     # BC: z = 0
     # -------------------------
+    # t_bc = torch.linspace(0, t_max/3+1, n_bc)
     t_bc = torch.rand(n_bc, 1) * t_max
     z_bc = torch.zeros(n_bc, 1)
     S_bc = torch.full((n_bc, 1), S0)
@@ -29,38 +37,81 @@ def generate_dataset(
     # IC: t = 0
     # -------------------------
     t_ic = torch.zeros(n_ic, 1)
+    # z_ic = torch.linspace(0, z_max/3+1, n_ic)
     z_ic = torch.rand(n_ic, 1) * z_max
     S_ic = torch.full((n_ic, 1), S0)
 
     # -------------------------
-    # Данные
+    # Данные для data-driven
     # -------------------------
-    t_data = torch.rand(n_pde, 1) * t_max
-    z_data = torch.rand(n_pde, 1) * z_max
+    t_train = torch.rand(n_data, 1) * t_max/2
+    z_train = torch.rand(n_data, 1) * z_max/2
 
     # аналитическое решение
-    tau = t_data - z_data / v_m
+    tau = t_train - z_train / v_m
 
-    S_true = torch.where(
+    S_train = torch.where(
         tau > 0,
         S_max - (S_max - S0) * torch.exp(-k * tau),
         torch.full_like(tau, S0)
     )
 
     # шум
-    noise = noise_std * torch.randn_like(S_true)
-    S_true_noisy = S_true + noise
+    noise = noise_std * torch.randn_like(S_train)
+    S_train_noisy = S_train + noise
+
+    # -------------------------
+    # Данные для тестирования
+    # -------------------------
+    # t_test = torch.rand(n_test, 1) * t_max
+    # z_test = torch.rand(n_test, 1) * z_max
+
+    t_test = torch.linspace(0, t_max, n_test)
+    z_test = torch.linspace(0, z_max, n_test)
+
+    T, Z = torch.meshgrid(t_test, z_test, indexing='ij')
+
+    # аналитическое решение
+    tau = T - Z / v_m
+
+    S = torch.where(
+        tau > 0,
+        S_max - (S_max - S0) * torch.exp(-k * tau),
+        torch.full_like(tau, S0)
+    )
+
+    # шум
+    noise = noise_std * torch.randn_like(S)
+    S_test_noisy = S + noise
 
     return {
+        # physics
         "t_pde": t_pde,
         "z_pde": z_pde,
+
+        # BC
         "t_bc": t_bc,
         "z_bc": z_bc,
         "S_bc": S_bc,
+
+        # IC
         "t_ic": t_ic,
         "z_ic": z_ic,
         "S_ic": S_ic,
-        "t_data": t_data,
-        "z_data": z_data,
-        "S_true": S_true_noisy
+
+        # data-driven
+        "t_train": t_train,
+        "z_train": z_train,
+        "S_train": S_train_noisy.view(n_data, 1),
+
+        # testing
+        "t_test": t_test,
+        "z_test": z_test,
+        "S_test": S_test_noisy,
+
+        # for plots
+        "T": T,
+        "Z": Z,
+        "S": S,
+
     }
